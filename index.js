f74a221c80ab4c0cc8078581003482514f9cb678
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-12-29 12:22:45 +0600

    update a documents and create get api for complete task

--- index.js
+++ index.js
@@ -25,28 +25,58 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const AllTask = client.db('TaskManager').collection('alltask')
-        app.post('/alltask', async(req, res) =>{
+        app.post('/alltask', async (req, res) => {
             const task = req.body
             const result = await AllTask.insertOne(task)
             res.send(result)
         })
-        app.get('/alltask', async(req, res) =>{
+        app.get('/alltask', async (req, res) => {
             const query = req.query
             const filter = {
-                email: query.email
+                email: query?.email,
+                completeStatus: false 
             }
-            const cursor = await AllTask.find(query).toArray()
+            console.log(filter)
+            const cursor = await AllTask.find(filter).toArray()
             res.send(cursor)
         })
+        // create api from incomplete to complete task 
+        app.patch('/alltask/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = {
+                _id: ObjectId(id)
+            }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    completeStatus: true
+                }
+            }
+            const result = await AllTask.updateOne(filter, updateDoc, option)
+            res.send(result)
+        })
 
-        app.delete('/alltask/:id', async(req, res) =>{
+        // delete an item 
+        app.delete('/alltask/:id', async (req, res) => {
             const id = req.params.id
             const filter = {
                 _id: ObjectId(id)
             }
             const result = await AllTask.deleteOne(filter)
             res.send(result)
-            
+        })
+
+
+        // complete task functionalities
+        // create api for complete task 
+        app.get('/completed', async(req, res) =>{
+            const email = req.query.email
+            const filter = {
+                email: email,
+                completeStatus: true
+            }
+            const result = await AllTask.find(filter).toArray()
+            res.send(result)
         })
     }
     catch {
